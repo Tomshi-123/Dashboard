6a759bebb180c0287d48c4a6c9015598184fcb7a
alexander <alexander.t.simonsen@gmail.com>
2025-03-31 22:01:26 +0200

    Sparcommit

---
+++ main.js
@@ -0,0 +1,8 @@
+import "./links.js";
+import "./time.js";
+
+
+// Om du vill skriva något direkt när DOM är laddad:
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Alla skript är nu laddade!");
+});
