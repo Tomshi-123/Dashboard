80bae0f3639b871f59a4d69996b74beaeaecec30
alexander <alexander.t.simonsen@gmail.com>
2025-04-09 14:11:13 +0200

    finishPush2

---
+++ js/main.js
@@ -0,0 +1,13 @@
+import "./time.js";          
+import "./randomImage.js";   
+import "./title.js";         
+import "./links.js";         
+import "./notes.js";         
+import "./weather.js";       
+import "./news.js";          
+
+
+// Om du vill skriva något direkt när DOM är laddad:
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Alla skript är nu laddade!");
+});
